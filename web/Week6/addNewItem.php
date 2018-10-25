e00d634e5975cfa4d62af19db84f4440083117ea
eversolea <austin@eversoles.org>
2018-10-25 12:08:14 -0600

    ashlee do u love me

--- web/Week6/addNewItem.php
+++ web/Week6/addNewItem.php
@@ -12,61 +12,7 @@ session_start();
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-          
-
-  <script>
-    document.getElementById("changeForm").onclick = function(){updateForm()};
-    function updateForm() {
-        document.getElementById("test").innerHTML = "yoyoyo";
-        var d = document.getElementById("myDropdown");
-        var table = d.options[e.selectedIndex].value;
-        
-        if(table = "1")
-        {
-            document.getElementById("EntryFormTitle").innerHTML = "Entry Form: Costumes";
-            document.getElementById("EntryForm")innerHTML = "Name: <input type='text' name='name' value=''><br />
-            Price: <input type='text' name='price' value=''><br />
-            Item Size: <input type='text' name='size' value=''><br />
-            Webpage URL: <input type='text' name='website' value=''><br />
-            Thumbnail Image URL: <input type='text' name='image' value=''><br />
-            <input type='submit'>";
-        }
-        else if(table = "2")
-        {
-            document.getElementById("EntryFormTitle").innerHTML = "Entry Form: Games";
-            document.getElementById("EntryForm")innerHTML = "Name: <input type='text' name='name' value=''><br />
-            Price: <input type='text' name='price' value=''><br />
-            Year Released: <input type='text' name='year' value=''><br />
-            Synopsis: <input type='text' name='synopsis' value=''><br />
-            Rating: <input type='text' name='rating' value=''><br />
-            Webpage URL: <input type='text' name='website' value=''><br />
-            Thumbnail Image URL: <input type='text' name='image' value=''><br />
-            <input type='submit'>";
-        }
-        else if(table = "3")
-        {
-            document.getElementById("EntryFormTitle").innerHTML = "Entry Form: Toys";
-            document.getElementById("EntryForm")innerHTML = "Name: <input type='text' name='name' value=''><br />
-            Price: <input type='text' name='price' value=''><br />
-            Item Size: <input type='text' name='size' value=''><br />
-            For Kids? (Y/N): <input type='text' name='kids' value=''><br />
-            Webpage URL: <input type='text' name='website' value=''><br />
-            Thumbnail Image URL: <input type='text' name='image' value=''><br />
-            <input type='submit'>";
-        }
-        else if(table = "4")
-        {
-            document.getElementById("EntryFormTitle").innerHTML = "Entry Form: Other";
-            document.getElementById("EntryForm")innerHTML = "Name: <input type='text' name='name' value=''><br />
-            Price: <input type='text' name='price' value=''><br />
-            Item Size: <input type='text' name='size' value=''><br />
-            Webpage URL: <input type='text' name='website' value=''><br />
-            Thumbnail Image URL: <input type='text' name='image' value=''><br />
-            <input type='submit'>";
-        }
-    }
-  
-  </script>
+         
  
   <style>
     /* source: https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_temp_store&stacked=h */
@@ -127,21 +73,11 @@ session_start();
       <div class="panel panel-primary">
         <div class="panel-heading"><h2>Do you have a new item to add?</h2></div>
         <div class="panel-body">
-        
-        <div id="test">
-
-        <button id="changeForm">Click to update entry form</button>
-        <select id="myDropdown">
-            <option selected="selected" value="1">Costumes</option>
-            <option value="2">Games</option>
-            <option value="3">Toys</option>
-            <option value="4">Other</option>
-        </select>
-
 
         <br />
         <h3 id="EntryFormTitle"> Entry Form: Costumes </h3>
-        <form id="EntryForm" action="addNewItem.php" method="post">
+        <form id="EntryForm" action="addNewItem.php" method="post">'
+            <input type="hidden" name="itemType" value="0" />
             Name: <input type="text" name="name" value=""><br />
             Price: <input type="text" name="price" value=""><br />
             Item Size: <input type="text" name="size" value=""><br />
@@ -149,10 +85,70 @@ session_start();
             Thumbnail Image URL: <input type="text" name="image" value=""><br />
             <input type="submit">
         </form>
-        </div>
+        
+        <h3 id="EntryFormTitle"> Entry Form: Games </h3>
+        <form id="EntryForm2" action="addNewItem.php" method="post">
+            <input type="hidden" name="itemType" value="1" />
+            "Name: <input type='text' name='name' value=''><br />
+            Price: <input type='text' name='price' value=''><br />
+            Year Released: <input type='text' name='year' value=''><br />
+            Synopsis: <input type='text' name='synopsis' value=''><br />
+            Rating: <input type='text' name='rating' value=''><br />
+            Webpage URL: <input type='text' name='website' value=''><br />
+            Thumbnail Image URL: <input type='text' name='image' value=''><br />
+            <input type='submit'>
+        </form>  
+            
+        <h3 id="EntryFormTitle"> Entry Form: Toys </h3>
+        <form id="EntryForm3" action="addNewItem.php" method="post">
+            <input type="hidden" name="itemType" value="2" />
+            Name: <input type='text' name='name' value=''><br />
+            Price: <input type='text' name='price' value=''><br />
+            Item Size: <input type='text' name='size' value=''><br />
+            For Kids? (Y/N): <input type='text' name='kids' value=''><br />
+            Webpage URL: <input type='text' name='website' value=''><br />
+            Thumbnail Image URL: <input type='text' name='image' value=''><br />
+            <input type='submit'>
+        </form>
+
+        <h3 id="EntryFormTitle"> Entry Form: Other </h3>
+        <form id="EntryForm4" action="addNewItem.php" method="post">
+            <input type="hidden" name="itemType" value="3" />
+            Name: <input type='text' name='name' value=''><br />
+            Price: <input type='text' name='price' value=''><br />
+            Item Size: <input type='text' name='size' value=''><br />
+            Webpage URL: <input type='text' name='website' value=''><br />
+            Thumbnail Image URL: <input type='text' name='image' value=''><br />
+            <input type='submit'>
+         </form>
+
         
         <?php
+        
+        if($_POST["itemType"] = '0') {}
+            echo "INSERT INTO costumes (name,price,isize,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["website"].", ".$_POST["image"].");";
+            #$statement1 = $db->prepare("INSERT INTO costumes (name,price,isize,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["website"].", ".$_POST["image"].");");
+            #$statement1->execute();
+        }      
+        
+        if($_POST["itemType"] = '1') {}
+            echo "INSERT INTO games (name,price,yearreleased,synopsis,rating,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["year"].",".$_POST["synopsis"].",".$_POST["rating"].",".$_POST["website"].", ".$_POST["image"].");";
+            #$statement1 = $db->prepare("INSERT INTO costumes (name,price,isize,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["website"].", ".$_POST["image"].");");
+            #$statement1->execute();
+        }
 
+        if($_POST["itemType"] = '2') {}
+            echo "INSERT INTO toys (name,price,isize,forkids,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["size"].", ".$_POST["kids"].",".$_POST["website"].", ".$_POST["image"].");";
+            #$statement1 = $db->prepare("INSERT INTO costumes (name,price,isize,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["website"].", ".$_POST["image"].");");
+            #$statement1->execute();
+        }
+        
+        if($_POST["itemType"] = '3') {}
+            echo "INSERT INTO other (name,price,isize,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["size"].",".$_POST["website"].", ".$_POST["image"].");";
+            #$statement1 = $db->prepare("INSERT INTO costumes (name,price,isize,website,image) VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["website"].", ".$_POST["image"].");");
+            #$statement1->execute();
+        }
+        
         ?>
         </div>
         <div class="panel-footer">
