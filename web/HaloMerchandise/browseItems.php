82f62c49a4f98d2816803693b4fd4511f62c321f
eversolea <austin@eversoles.org>
2018-11-04 12:14:09 -0700

    post to get

--- web/HaloMerchandise/browseItems.php
+++ web/HaloMerchandise/browseItems.php
@@ -37,7 +37,7 @@ require('dbConnect.php')
 <body>
 <?php
 
-foreach ($_POST as $param_name => $param_val) {
+foreach ($_GET as $param_name => $param_val) {
     echo "Param: $param_name; Value: $param_val<br />\n";
 }
 
