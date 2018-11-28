2c60956490939a84de5463269d43d2ae2fe32176
eversolea <austin@eversoles.org>
2018-11-27 21:22:14 -0700

    yeah buddy

--- web/Week10/index.js
+++ web/Week10/index.js
@@ -10,47 +10,65 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/math', handleMath);
+app.get('/letter', handleLetter);
 
 app.listen(app.get('port'), function() {
-  console.log('Austin's Node app is running on port', app.get('port'));
+  console.log('Austins Node app is running on port', app.get('port'));
 });
 
-function handleMath(request, response) {
-	var operation = request.query.operation;
-	var operand1 = Number(request.query.operand1);
-	var operand2 = Number(request.query.operand2);
-
+function handleLetter(request, response) {
+	var letterType = request.query.letter;
+    var weight = request.query.weight;
+    var cost = 0;
+    if(letterType == "Letters (Stamped)") {
+        if(weight <= 1) { cost = 0.5;}
+        else if(weight <= 2) { cost = 0.71;}
+        else if(weight <= 3) { cost = 0.92;}
+        else if(weight <= 3.5){ cost = 1.13;}
+        else { letterType="Large Envelopes (Flats)";}
+    }
+    if(letterType == "Letters (Metered)") {
+        if(weight <= 1) { cost = 0.47;}
+        else if(weight <= 2) { cost = 0.68;}
+        else if(weight <= 3) { cost = 0.89;}
+        else if(weight <= 3.5){ cost = 1.10;}
+        else { letterType="Large Envelopes (Flats)";}
+    }   
+    if(letterType == "Large Envelopes (Flats)") {
+        if(weight <= 1) { cost = 1;}
+        else if(weight <= 2) { cost = 1.21;}
+        else if(weight <= 3) { cost = 1.42;}
+        else if(weight <= 4) { cost = 1.63;}
+        else if(weight <= 5) { cost = 1.84;}
+        else if(weight <= 6) { cost = 2.05;}
+        else if(weight <= 7) { cost = 2.26;}
+        else if(weight <= 8) { cost = 2.47;}
+        else if(weight <= 9) { cost = 2.68;}
+        else if(weight <= 10) { cost = 2.89;}
+        else if(weight <= 11) { cost = 3.10;}
+        else if(weight <= 12) { cost = 3.31;}
+        else {cost = 3.52;}
+    }
+    if(letterType == "First-Class Package Service-Retail") {
+        if(weight <= 1) { cost = 3.5;}
+        else if(weight <= 2) { cost = 3.5;}
+        else if(weight <= 3) { cost = 3.5;}
+        else if(weight <= 4) { cost = 3.5;}
+        else if(weight <= 5) { cost = 3.75;}
+        else if(weight <= 6) { cost = 3.75;}
+        else if(weight <= 7) { cost = 3.75;}
+        else if(weight <= 8) { cost = 3.75;}
+        else if(weight <= 9) { cost = 4.1;}
+        else if(weight <= 10) { cost = 4.45;}
+        else if(weight <= 11) { cost = 4.8;}
+        else if(weight <= 12) { cost = 5.15;}
+        else {cost = 5.5;}
+    }
+    
+    
+    var params = {cost: cost, weight: weight, letterType: letterType}
+    response.render('pages/result', params);
 	// TODO: Here we should check to make sure we have all the correct parameters
-
-	computeOperation(response, operation, operand1, operand2);
-}
-
-function computeOperation(response, op, left, right) {
-	op = op.toLowerCase();
-
-	var result = 0;
-
-	if (op == "add") {
-		result = left + right;
-	} else if (op == "subtract") {
-		result = left - right;		
-	} else if (op == "multiply") {
-		result = left * right;
-	} else if (op == "divide") {
-		result = left / right;
-	} else {
-		// It would be best here to redirect to an "unknown operation"
-		// error page or something similar.
-	}
-
-	// Set up a JSON object of the values we want to pass along to the EJS result page
-	var params = {operation: op, left: left, right: right, result: result};
-
-	// Render the response, using the EJS page "result.ejs" in the pages directory
-	// Makes sure to pass it the parameters we need.
-	response.render('pages/result', params);
-
 }
 
 
