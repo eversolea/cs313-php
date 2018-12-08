79a1b3842c3e0422ee7d44a5def63b84976c0a47
eversolea <austin@eversoles.org>
2018-12-08 14:50:03 -0700

    added more js ajax functions

--- web/Final/AustDirectory/js/hipster.js
+++ web/Final/AustDirectory/js/hipster.js
@@ -8,3 +8,15 @@ function loadCollection() {
   xhttp.open("GET", "newItems.html", true);
   xhttp.send();
 }
+
+
+function loadMainScreen() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("mainView").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "mainScreen.html", true);
+  xhttp.send();
+}
