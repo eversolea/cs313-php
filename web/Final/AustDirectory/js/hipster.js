a5419803c177c9d56419247332b4f7650e4d0606
eversolea <austin@eversoles.org>
2018-12-07 19:05:29 -0700

    k

--- web/Final/AustDirectory/js/hipster.js
+++ web/Final/AustDirectory/js/hipster.js
@@ -2,8 +2,7 @@ function loadCollection() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
+      document.getElementById("mainView").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "newItems.html", true);
