a5ed5938085fdbeb20eff331f130198d8cdc678a
eversolea <austin@eversoles.org>
2018-12-07 19:01:54 -0700

    try this

---
+++ web/Final/AustDirectory/js/hipster.js
@@ -0,0 +1,11 @@
+function loadCollection() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "newItems.html", true);
+  xhttp.send();
+}
